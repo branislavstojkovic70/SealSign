3d1bbf120977d60178b1096b3efc26d013b14dde
branislavstojkovic70 <96660978+branislavstojkovic70@users.noreply.github.com>
2026-04-04 10:47:43 +0200

    feat(frontend-init): changedroot configuration of css and main.tsx. Also routing and styling added, aswell as theme for the app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,46 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <div>Navbar</div>,
+		children: [
+			{
+				path: "/",
+
+			}
+		],
+	},
+	{
+		path: "/home",
+		element: <div>Home</div>,
+	},
+]);
+
+createRoot(document.getElementById("root")!).render(
+	<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<RouterProvider router={router} />
+			<Toaster
+				position="bottom-center"
+				toastOptions={{
+					success: {
+						style: {
+							background: theme.palette.success.main,
+						},
+					},
+					error: {
+						style: {
+							background: theme.palette.error.main,
+							color: "#F5F5F5",
+						},
+					},
+				}}
+			/>
+	</ThemeProvider>
+);
